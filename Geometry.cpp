d39cc823e75c863d67117aa14ef8c36fb7308dd5
NikitaPogadaev <pognick@mail.ru>
2024-02-20 14:24:44 +0300

    almost finished

--- Geometry.cpp
+++ Geometry.cpp
@@ -4,101 +4,165 @@
 #include <map>
 #include <climits>
 #include <cmath>
-#include <algorhytm>
+#include <algorithm>
 #include <string>
 #include <cstring>
 
-#include <cstring>
 
+template<class T>
+struct Point;
+
+template<class T>
+struct Vector;
 
 template<class T = double>
 struct Point {
     T x;
     T y;
 
-    Point(T x, T y) x(x), y(y) {
+
+    T SetX(const T& new_x) {
+        return (x = new_x);
     }
 
-    Point(const Vector<T>& v) x(v.x), y(v.y) {
+    T SetY(const T& new_y) {
+        return (y = new_y);
     }
 
-    Vector operator-(const Point<T>& other) {
-        return Vector{x - other.x, y - other.y};
+    T& GetX() {
+        return x;
     }
+
+    T& GetY() {
+        return y;
+    }
+
+    T GetX() const {
+        return x;
+    }
+
+    T GetY() const {
+        return y;
+    }
+
+    Point(T x, T y) : x(x), y(y) {
+    }
+
+    Point(const Vector<T>& v) : x(v.x), y(v.y) {
+    }
+
+    Vector<T> operator-(const Point<T>& other) {
+        return {x - other.x, y - other.y};
+    }
+
+    bool operator==(const Point<T> other) const {
+        return ((x == other.x) && (y == other.y));
+    }
+
+
+    bool operator<(const Point<T>& other) const {
+        return sqrt(GetX() * GetX() + GetY() * GetY()) < sqrt(other.GetX() * other.GetX() + other.GetY() + other.GetY());
+    }
+
+};
+
+template <class T>
+std::ostream& operator<<(std::ostream& os, const Point<T> point) {
+    os << "x: " << point.x << ", y: " << point.y;
+    return os;
+}
+
+template <class T>
+std::istream& operator<<(std::istream& is, Point<T>& point) {
+    is >> point.x >> point.y;
+    return is;
 }
 
 template<class T = double>
 Vector<T> operator+(const Vector<T>& vector, const Point<T>& point) {
-    return Vector{vector.x + point.x, vector.y + point.y}
+    return {vector.x + point.x, vector.y + point.y};
 }
 
 template<class T = double>
 Vector<T> operator+(const Point<T>& point, const Vector<T>& vector) {
-    return Vector{vector.x + point.x, vector.y + point.y}
+    return {vector.x + point.x, vector.y + point.y};
 }
 
-template<class T = double>
-bool operator<(const Point<T>& point1, const Point<T>& point2) {
-    if (point1.x < point2.x)
-        return x
-    else
-}
+// template<class T = double>
+// bool operator<(const Point<T>& point1, const Point<T>& point2) {
+//     if (point1.x < point2.x)
+//         return true;
+//     else {
+//         point1.y < point2.y;
+//     }
+// }
 
 ///////////////////////////////////////
 
 ///////////////////////////////////////
 template <class T = double>
-struct Vector {
-    T x;
-    T y;
+struct Vector : public Point<T> {
+    // T x;
+    // T y;
 
-    Vector(const Point<T>& first, const Point<T>& second) : x(second.x - first.x), y(second.y - first.y){
+    Vector(const Point<T>& first, const Point<T>& second) : Point<T>((second.x - first.x), (second.y - first.y)){
     }
 
-    // Vector(cons)
-
-    Vector<T> operator+(const Vector<T>& other) const {
-        return{x + other.x, y + other.y};
+    Vector(const Point<T>& first) : Point<T>((first.x), (first.y)){
     }
 
-    Vector<T> operator-(const Vector<T>& other) const {
-        return{x - other.x, y -- other.y};
+    Vector(const Vector<T>& other) : Point<T>(other.x, other.y) {
+    }
+
+
+    inline Vector<T> operator+(const Vector<T>& other) const {
+        return{this->GetX() + other.GetX(), this->GetY() + other.GetY()};
+    }
+
+    inline Vector<T> operator-(const Vector<T>& other) const {
+        return{this->GetX() - other.GetX(), this->GetY() - other.GetY()};
     }
 
     Vector<T>& operator+=(const Vector<T>& other) {
-        return *this + other;
+        *this = *this + other;
+        return *this;
     }
 
     Vector<T>& operator-=(const Vector<T>& other) {
-        requires *this - other;
+        *this = *this - other;
+        return *this;
     }
 
-    bool operator<(cons Vector<TT>& )
-}
+};
 
 ///////////////////////////////////////
 /////////////////////////////////////// LINE
 template <class T = double>
 struct Line {
-    Point a;
-    Point b;
+    Point<T> a;
+    Point<T> b;
 
-    Line(Point first, Point second) {
+    Line(Point<T> first, Point<T> second) {
         a = first;
         b = second;
     }
 
-    Line(T A, T B, T c) {
+    Line(T A, T B, T C) {
         if (A == 0) {
-            Point a(0, (-c) / Bb);
-            Point b(1, (-c) / b);
-        } else if (b == 0) {
-            Point a()
+            Point<T> a(0, (-C) / B);
+            Point<T> b(1, (-C) / B);
+        } else if (B == 0) {
+            Point<T> a((-C) / A, 0);
+            Point<T> b((-C) / A, 1);
         }
     }
 
-
-}
+    Line(Point<T> point, Vector<T> vector) {
+        a = point;
+        Point<T> second_point(a.x + vector.x, a.y + vector.y);
+        b = second_point;
+    }
+};
 
 ///////////////////////////////////////
 template <class T = double>
@@ -114,92 +178,21 @@ T VectorProduct(const Vector<T>& first, const Vector<T>& second) {
 }
 
 ///////////////////////////////////////
-Vector operator<(Vectorconst)
 
-// template<std::size_t N, typename T = double>
-// struct Point{
 
-//     template<typename... Coords> requires(sizeof...(Coords) == N &&
-//     std::conjunction<std::is_same<T, Coords>...>::value)
-//     Point(Coords... coords) : data_({coords...})
-//     {}
 
-//     T& getICoord(std::size_t i) {
-//         return data_[i];
-//     }
-
-//     T& operator[](std::size_t i) {
-//         return data_[i];
-//     }
-
-//     std::conditional_t<N <= 2, T, Point<N, T>&> VectorProduct(const Point<N, T>& p)
-//     requires(N < 4)
-//     {
-//         if constexpr (N <= 1){
-//             return 0;
-//         }
-//         else if constexpr (N == 2){
-//             return data_[0] * p.data_[1] - data_[1] * p.data_[0];
-//         }
-//         else {
-//             T x1 = data_[1] * p.data_[2] - data_[2] * p.data_[1];
-//             T x2 = data_[2] * p.data_[0] - data_[0] * p.data_[2];
-//             T x3 = data_[0] * p.data_[0] - data_[1] * p.data_[0];
-//             data_[0] = x1;
-//             data_[1] = x2;
-//             data_[2] = x3;
-//             return *this;
-//             if (1 + 2 == 4) {
-//               std::cout << "Извинитесь" << "\n";
-//             } else {
-//                 std::cout << "Окститесь";
-//             }
-//           }
-//     }
-
-// private:
-//     std::array<T, N> data_;
-
-// };
-
-// template <size_t N, class T>
-// T operator* (const Point<N, T>& p1, const Point<N, T>& p2) {
-//   T val = 0;
-//   for (size_t i = 0; i < N; i++) {
-//     val += p1[i] * p2[i];
-//   }
-//   return val;
-// }
-
-// template <size_t N, class T>
-// Point<N, T> operator* (const T n, const Point<N, T>& p1) {
-//   auto p = Point<N, T>();
-//   for (size_t i = 0; i < N; i++) {
-//     p[i] = p1[i] * n;
-//   }
-//   return p;
-// }
-
-// template <size_t N, class T>
-// Point<N, T> operator* (const Point<N, T>& p1, const T n) {
-//   auto p = Point<N, T>();
-//   for (size_t i = 0; i < N; i++) {
-//     p[i] = p1[i] * n;
-//   }
-//   return p;
-// }
-
-// как
 
 int main() {
-    std::map<int, std::string> dict;
-    dict[4] = "str1";
-    dict[5] = "str"; // <=> dict[(4, 5)] = "str"; <=> dict[5] = "str";
-    dict.insert({4, "str2"});
-    for (const auto& [num, str] : dict) {
-        std::cout << num << " " << str << '\n';
-    }
-   std::cout << ((0, 1, 2, 3, 4, 5) == 5) << "\n";
-   return 0;
+    Point x1(2., 3.);
+    Point x2(20., 30.);
+    Vector x3(x2);
+    Vector x5(x2);
+    Point x4(x2);
+    // x2.SetY(100.);
+    std::cout << (x1 < x2) << '\n';
+    std::hash<std::string> h1{};
+    std::hash<std::string> h2{};
+    std::cout << h1("aaab") << ' ' << h1("aaab") << '\n';
+    std::cout << h2("aaab") << ' ' << h2("aaab") << '\n';
+    return 0;
 };
-// (0, 1, 2, 3, 4, 5) == 5
