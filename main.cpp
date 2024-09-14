fdef4c35522fd737f64a7a35b8feb1f2e139e24d
NikitaPogadaev <pognick@mail.ru>
2024-09-14 19:52:53 +0300

    undetected old changes

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
-#include "GraphAlgo.h"
-#include "Graph.h"
+// #include "GraphAlgo.h"
+// #include "Graph.h"
 
 #include <iostream>
+#include <vector>
+#include <set>
+
+template<typename T>
+concept Iterable = requires(T t) {
+    // Проверка наличия методов begin() и end()
+    { t.begin() } -> std::same_as<typename T::iterator>;
+    { t.end() } -> std::same_as<typename T::iterator>;
+};
+
+void foo(auto& cont){
+    for(auto& i:cont){
+        std::cout << i << ' ';
+    }
+}
+
+template<Iterable T>
+std::ostream& operator<<(std::ostream& os, T& cont){
+for (auto& v : cont) {
+    os << v << "\n";
+  }
+  return os;
+}
 
 int main() {
-    WeightedGraph g;
-    g.InsertVertex();
-    std::cout << 1;
+    std::set<int> v{1, 4, 2, 4};
+    std::cout << v;
+    foo(v);
+    
 }
